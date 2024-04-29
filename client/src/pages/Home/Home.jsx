80e04432a5142fabf8aacaa8bf9ea4a6b43e2744
Deniz Yetim <denizyetim@hotmail.com>
2024-04-29 20:50:05 +0300

    Add files via upload

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../../components/Global/Navbar/Navbar"
+import { HomeContainer } from "./Home.style"
+
+export function Home(){
+
+    return (
+        <HomeContainer>
+            <Navbar/>
+        </HomeContainer>
+    )
+}
---
+++ client/src/pages/Home/Home.style.js
@@ -0,0 +1,12 @@
+import {styled} from "styled-components";
+
+export const HomeContainer =  styled.div`
+    background-color: gray;
+    width: 100vw;
+    height: 100vh;
+    //background-color: yellow;
+    display: flex;
+    justify-content: center;
+    
+
+`
