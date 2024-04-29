6924c83cd78ff423406f036ef3faca9415a509d0
Deniz Yetim <denizyetim@hotmail.com>
2024-04-29 17:32:53 +0300

    Add files via upload

---
+++ school-project-github/client/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../../components/Home/Navbar/Navbar"
+import { HomeContainer  } from "./Home.style"
+
+export function Home(){
+
+    
+
+    return (
+        <HomeContainer>
+            <Navbar/>
+        </HomeContainer>
+    )
+}
---
+++ school-project-github/client/src/pages/Home/Home.style.js
@@ -0,0 +1,12 @@
+import {styled} from "styled-components";
+
+export const HomeContainer =  styled.div`
+
+    padding-top: 25px;
+    width: 100%;
+    background-color: yellow;
+    display: flex;
+    justify-content: center;
+    
+
+`
