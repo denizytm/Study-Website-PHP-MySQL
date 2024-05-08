ca99f0a1b1ca21051b7f517f65c280845886275b
Deniz Yetim <denizyetim@hotmail.com>
2024-05-08 21:33:17 +0300

    Delete views directory

--- views/partials/body/contactBody.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-    require "./views/components/global/navbar.php";
-?>
-<h1>CONTACT US</h1>
-<?php 
-    require "./views/components/global/footer.php";
-?>
--- views/partials/body/questionsBody.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-    require "./views/components/global/navbar.php";
-?>
-<h1>QUESTIONS</h1>
-<?php 
-    require "./views/components/global/footer.php";
-?>
