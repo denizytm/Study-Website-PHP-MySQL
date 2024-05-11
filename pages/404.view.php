e1d5b71afd95f7507d2a85d76b8ae20d18a9ea8a
Deniz Yetim <denizyetim@hotmail.com>
2024-05-11 10:01:53 +0300

    Delete pages directory

--- pages/404.view.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$headData = partialsHead("404Head.php");
-$bodyData = partialsBody("404Body.php");
-
-require globalPartials("head.php");
-require globalPartials("body.php");
--- pages/home.view.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$headData = partialsHead("homeHead.php");
-$bodyData = partialsBody("homeBody.php");
-
-require globalPartials("head.php");
-require globalPartials("body.php");
