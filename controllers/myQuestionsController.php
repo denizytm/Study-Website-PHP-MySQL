8b22a09997d49489c7892958a789891c82145f6e
Deniz Yetim <denizyetim@hotmail.com>
2024-05-13 22:30:36 +0300

    Delete controllers directory

--- controllers/myQuestionsController.php
+++
@@ -1,3 +0,0 @@
-<?php 
-
-require pages("myQuestions.view.php");
--- controllers/navbarController.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-if(isset($_SERVER["PATH_INFO"])) $requestData = $_SERVER["PATH_INFO"];
-else $requestData = $_SERVER["REQUEST_URI"]; 
-
-require components("global/navbar.php");
