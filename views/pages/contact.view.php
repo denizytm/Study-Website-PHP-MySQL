3d5db6a373bf849fd055de6d41d0ae7946f7f7a4
Deniz Yetim <denizyetim@hotmail.com>
2024-05-14 22:29:03 +0300

    Delete views directory

--- views/pages/contact.view.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$headData = partialsHead("contactHead.php");
-$bodyData = partialsBody("contactBody.php");
-
-require globalPartials("head.php");
-require globalPartials("body.php");
--- views/pages/login.view.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$headData = partialsHead("loginHead.php");
-$bodyData = partialsBody("loginBody.php");
-
-require globalPartials("head.php");
-require globalPartials("body.php");
