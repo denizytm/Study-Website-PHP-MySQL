e23b5a194e06c568d67435c2c904d227b932d1df
Deniz Yetim <denizyetim@hotmail.com>
2024-05-16 14:34:04 +0300

    Add files via upload

---
+++ views/pages/questions.view.php
@@ -0,0 +1,9 @@
+<?php
+
+$headData = partialsHead("questionsHead.php");
+$bodyData = partialsBody("questionsBody.php");
+
+$searchData = "";
+
+require globalPartials("head.php");
+require globalPartials("body.php");
---
+++ views/pages/register.view.php
@@ -0,0 +1,7 @@
+<?php
+
+$headData = partialsHead("registerHead.php");
+$bodyData = partialsBody("registerBody.php");
+
+require globalPartials("head.php");
+require globalPartials("body.php");
